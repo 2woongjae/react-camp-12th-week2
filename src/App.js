b8ca66fc87f26c379b174171dc080ace0d68dc52
Mark Lee <mark@protopie.io>
2020-07-18 14:51:30 +0900

    7월 18일 14시 50분

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ClassComponent from "./components/ClassComponent";
+import FunctionalComponent from "./components/FunctionalComponent";
+
+// 1. flow
+// 2. TypeScript
 
 function App() {
+  // const props = {name: 'Mark', age: 38};
+
+  // 1. {name: 'Mark', age: 38, children: 자식들} => props
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ClassComponent namu="Mark" age={38} isFemale={true}>
+        자식들
+      </ClassComponent>
+      <FunctionalComponent name="Anna" age={27} />
+      <FunctionalComponent name="Mark" age={38}>
+        강아지
+      </FunctionalComponent>
     </div>
   );
 }
